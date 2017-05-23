abacd18adb7e457669d0c890e64b7c791ae6950e
Andrew Schofield <the.uncle.fungus@gmail.com>
2017-05-23 11:18:03 +0100

    Fix an issue with the password last modified algorithm

--- HaveIBeenPwned/Extensions/ProtectedStringExtensions.cs
+++ HaveIBeenPwned/Extensions/ProtectedStringExtensions.cs
@@ -1,18 +1,19 @@
 ﻿using KeePassLib.Security;
 using KeePassLib.Utility;
+using System.Linq;
 
 namespace HaveIBeenPwned.Extensions
 {
     public static class ProtectedStringExtensions
     {
-        public static bool Equals(this ProtectedString protectedString, ProtectedString compareWith)
+        public static bool ValueEquals(this ProtectedString protectedString, ProtectedString compareWith)
         {
             // extract the unencrypted strings
             byte[] string1 = protectedString.ReadUtf8();
             byte[] string2 = compareWith.ReadUtf8();
 
             // compare the results
-            var result = string1 == string2;
+            var result = string1.SequenceEqual(string2);
 
             // clean up the byte arrays so we don't leak data
             MemUtil.ZeroByteArray(string1);
--- HaveIBeenPwned/Extensions/PwEntryExtensions.cs
+++ HaveIBeenPwned/Extensions/PwEntryExtensions.cs
@@ -12,17 +12,21 @@ namespace HaveIBeenPwned.Extensions
         {
             if (entry.History != null && entry.History.Any())
             {
-                var sortedEntries = entry.History.OrderByDescending(h => h.LastModificationTime);
-                foreach (var historyEntry in sortedEntries)
+                var sortedEntries = entry.History.OrderByDescending(h => h.LastModificationTime).ToList();
+                for (var i = 0; i < sortedEntries.Count(); i++)
                 {
-                    if(!entry.Strings.GetSafe(PwDefs.PasswordField).Equals(historyEntry.Strings.GetSafe(PwDefs.PasswordField)))
+                    if (!entry.Strings.GetSafe(PwDefs.PasswordField).ValueEquals(sortedEntries[i].Strings.GetSafe(PwDefs.PasswordField)))
                     {
-                        return historyEntry.LastModificationTime;
+                        // the last time the password was changed was the entry before the current one (ordered new to old)
+                        return i == 0 ? entry.LastModificationTime : sortedEntries[i - 1].LastModificationTime;
                     }
                 }
+
+                // if the password is the same in the entry and all history items all we know is that it is at least as old as the earliest history item
                 return sortedEntries.Last().LastModificationTime;
             }
 
+            // no history means either the password has just been set, or the history has been pruned in which case we have no other data
             return entry.LastModificationTime;
         }
 
